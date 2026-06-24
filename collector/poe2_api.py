cebfcc663d74e1009a7df3099c1824ecbac03a75
TSPbest <b.e.s.t.t.y.4842@gmail.com>
2026-06-24 22:43:16 +0700

    POE2 Assistant v1.0

--- collector/poe2_api.py
+++ collector/poe2_api.py
@@ -4,14 +4,8 @@ from collector.api_client import APIClient
 class POE2API:
 
     def __init__(self):
-
         self.client = APIClient()
 
     def get_build(self, url):
 
-        response = self.client.get(url)
-
-        if response is None:
-            return None
-
-        return response.json()
+        return self.client.get(url)
---
+++ config/settings.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://www.pathofexile.com/api/trade2"
+USER_AGENT = "POE2Assistant/1.0"
+TIMEOUT = 10
